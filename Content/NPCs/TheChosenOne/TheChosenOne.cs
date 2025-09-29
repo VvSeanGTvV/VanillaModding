e9e939e6d71b0dcbdcf1f5a989f10f0c6f5afbf5
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2025-09-28 21:45:15 -0700

    one himself (wip)

---
+++ Content/NPCs/TheChosenOne/TheChosenOne.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using VanillaModding.Content.Projectiles.OcramProjectile;
+
+namespace VanillaModding.Content.NPCs.TheChosenOne
+{
+    internal class TheChosenOne : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = 11;
+            NPCID.Sets.TrailCacheLength[Type] = 10;
+            NPCID.Sets.TrailingMode[Type] = 0;
+
+            // Add this in for bosses that have a summon item, requires corresponding code in the item (See MinionBossSummonItem.cs)
+            NPCID.Sets.MPAllowedEnemies[Type] = true;
+            // Automatically group with other bosses
+            NPCID.Sets.BossBestiaryPriority.Add(Type);
+
+            // Specify the debuffs it is immune to. Most NPCs are immune to Confused.
+            NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.OnFire] = true;
+            // This boss also becomes immune to OnFire and all buffs that inherit OnFire immunity during the second half of the fight. See the ApplySecondStageBuffImmunities method.
+
+            // Influences how the NPC looks in the Bestiary
+            float Scale = 0.5f;
+            NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            {
+                //CustomTexturePath = "ExampleMod/Assets/Textures/Bestiary/MinionBoss_Preview",
+                PortraitScale = Scale, // Portrait refers to the full picture when clicking on the icon in the bestiary
+                PortraitPositionYOverride = -((NPC.height / 2f + 7f) * Scale),
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+        }
+
+        public override void SetDefaults()
+        {
+            //90x82 FRAME
+            NPC.width = 82;
+            NPC.height = 90;
+
+            NPC.damage = 105;
+            NPC.defense = 20;
+            NPC.lifeMax = 200000;
+
+            NPC.HitSound = SoundID.NPCHit4;
+            NPC.DeathSound = SoundID.NPCDeath1;
+
+            NPC.noGravity = true;
+            NPC.noTileCollide = false;
+            NPC.value = Item.buyPrice(0, 0, 0, 0);
+            NPC.aiStyle = -1;
+            NPC.scale = 1f;
+
+            NPC.knockBackResist = 0;
+            NPC.boss = true;
+
+            if (!Main.dedServ)
+            {
+                Music = MusicID.OtherworldlyBoss1; //MusicLoader.GetMusicSlot(Mod, "Music/Ocram");
+            }
+            //ScaleStats();
+        }
+
+        int stg = 0, timer = 0, timer1 = 0;
+        bool onStand = true, battleStart = false, onGround;
+        int Frame = 0;
+        public override void AI()
+        {
+            onGround = false;
+            if (onStand)
+            {
+                NPC.ai[0] += 1f;
+                if (NPC.ai[0] >= 5f)
+                {
+                    NPC.ai[0] = 5f;
+                    NPC.velocity.Y += 0.1f;
+                }
+                if (Collision.SolidCollision(NPC.position, NPC.width, NPC.height))
+                {
+                    NPC.velocity.Y = 0;
+                    onGround = true;
+                }
+                if (NPC.velocity.Y > 16f) NPC.velocity.Y = 16f;
+            }
+
+            timer++;
+            if (onStand && timer > 60 * 3)
+            {
+                Frame = 1;
+                onStand = false;
+            } 
+            else if (timer > 60 * 3.15f)
+            {
+
+                if (!battleStart)
+                {
+                    NPC.velocity.Y -= 16f;
+                } 
+                else
+                {
+                    if (NPC.velocity.Y > -0.1f && NPC.velocity.Y < 0.1f) NPC.velocity.Y = 0f;
+                    NPC.velocity.Y *= 0.95f;
+                    if (Frame < 4)
+                    {
+                        int frameSpeed = 2;
+                        NPC.frameCounter += 0.5f;
+                        if (NPC.frameCounter > frameSpeed)
+                        {
+                            NPC.frameCounter = 0;
+                            Frame++;
+                        }
+                    }
+                }
+
+                if (timer > 60 * 5) FirstStage();
+                battleStart = true;
+            }
+        }
+
+        public void FirstStage()
+        {
+            var source = NPC.GetSource_FromAI();
+
+            timer1++;
+            if (stg == 0)
+            {
+                if (timer1 > 30)
+                {
+                    Vector2 direction = Vector2.UnitY * 10f; // Adjust speed as needed
+                    if (Main.netMode != NetmodeID.MultiplayerClient) Projectile.NewProjectile(source, NPC.Center - new Vector2(5, 20).RotatedBy(-0.25f * (Frame - 7)), direction.RotatedBy(-0.25f * (Frame - 7)), ModContent.ProjectileType<RedLaser>(), 37, 8);
+                    if (Frame < 8)
+                    {
+                        int frameSpeed = 2;
+                        NPC.frameCounter += 0.5f;
+                        if (NPC.frameCounter > frameSpeed)
+                        {
+                            NPC.frameCounter = 0;
+                            Frame++;
+                        }
+                    }
+                    else
+                    {
+
+                        int frameSpeed = 2;
+                        NPC.frameCounter += 0.5f;
+                        if (NPC.frameCounter > frameSpeed)
+                        {
+                            stg++;
+                        }
+                    }
+                }
+                else if (timer1 < 10)
+                {
+                    Frame = 3;
+                } 
+                else
+                {
+                    Frame = 6;
+                }
+            } 
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            /*int startFrame = 0;
+            int finalFrame = 3;
+
+            int frameSpeed = 2;
+            NPC.frameCounter += 0.5f;
+            NPC.frameCounter += NPC.velocity.Length() / 10f; // Make the counter go faster with more movement speed
+            if (NPC.frameCounter > frameSpeed)
+            {
+                NPC.frameCounter = 0;
+                NPC.frame.Y += frameHeight;
+
+                if (NPC.frame.Y > finalFrame * frameHeight)
+                {
+                    NPC.frame.Y = startFrame * frameHeight;
+                }
+            }*/
+            NPC.frame.Y = Frame * frameHeight;
+        }
+    }
+}
