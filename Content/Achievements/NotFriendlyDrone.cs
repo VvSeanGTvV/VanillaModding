b54827b32c97ac83ece441b99d42bfaa16548886
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2025-10-03 08:00:40 -0700

    a

--- Content/Achievements/NotFriendlyDrone.cs
+++ Content/Achievements/NotFriendlyDrone.cs
@@ -19,21 +19,8 @@ namespace VanillaModding.Content.Achievements
 
         public override void SetStaticDefaults()
         {
-            // There are 4 AchievementCategory options: Slayer, Collector, Explorer, and Challenger.
-            // Slayer is the default.
-            // If you want to change the achievement's category, you can do this:
-            // Achievement.SetCategory(AchievementCategory.Collector);
-
-            // Unlike MinionBossKilled, which uses AddNPCKilledCondition, this ModAchievement uses AddIntCondition to track the 5 kills. This is necessary because AddNPCKilledCondition only supports tracking a single kill.
             Condition = AddIntCondition(100);
-
-            // This approach requires manually incrementing Condition.Value to track the kill count. To do this, we subscribe to the AchievementsHelper.OnNPCKilled event, then in NPCKilledListener we increment our Condition by 1. See the NPCKilledListener method below.
-            // We can't use the ExampleWormHead.OnKill method for this because that doesn't run on multiplayer clients.
-            // The AchievementsHelper.OnNPCKilled event, however, properly runs for all clients that participated in a fight and damaged the NPC.
             AchievementsHelper.OnNPCKilled += NPCKilledListener;
-
-            // Other AchievementCondition options include: AddCondition, AddFloatCondition, AddItemCraftCondition, AddItemPickupCondition, AddNPCKilledCondition, and AddTileDestroyedCondition.
-            // If making a custom condition (AddCondition, AddFloatCondition, AddItemCraftCondition), make sure the logic is correct for multiplayer. Syncing effects through ModPacket might be required. Achievements are not loaded on the server so be sure to only increment custom condition values on clients to avoid null exceptions.
         }
 
         public override void Unload()
