9bf5a77390a07335c84c99cd6f22dcf6340bc144
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2025-12-17 11:48:42 -0800

    a

--- Content/Items/Consumable/Healing/Cantaloupe.cs
+++ Content/Items/Consumable/Healing/Cantaloupe.cs
@@ -14,22 +14,25 @@ namespace VanillaModding.Content.Items.Consumable.Healing
     {
         public override void SetDefaults()
         {
-            int width = 30; int height = width;
+            int width = 32; int height = 18;
             Item.Size = new Vector2(width, height);
 
             Item.useTime = Item.useAnimation = 17;
             Item.holdStyle = ItemHoldStyleID.HoldFront;
+            Item.useTurn = true;
 
-            Item.healLife = 120;
+            Item.healLife = 75;
             Item.potion = true;
+            Item.potionDelay = 2100;
+            Item.maxStack = Item.CommonMaxStack;
 
             Item.useStyle = ItemUseStyleID.EatFood;
-            Item.consumable = false;
+            Item.consumable = true;
             Item.noMelee = true;
 
             Item.value = Item.sellPrice(gold: 1, silver: 80);
             Item.rare = ItemRarityID.Orange;
-            Item.expert = true;
+            //Item.expert = true;
 
             Item.UseSound = SoundID.Item2;
         }
