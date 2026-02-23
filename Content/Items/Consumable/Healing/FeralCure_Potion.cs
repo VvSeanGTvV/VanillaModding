02488bb9d2d0dfff8ce25fdbe0803dedc70a9c87
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2026-02-22 22:36:32 -0800

    I added it too far late
    the series already ended lol

---
+++ Content/Items/Consumable/Healing/FeralCure_Potion.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VanillaModding.Content.Items.Consumable.Healing
+{
+    internal class FeralCure_Potion : ModItem
+    {
+        public override void SetDefaults()
+        {
+            int width = 20; int height = 26;
+            Item.Size = new Vector2(width, height);
+
+            Item.useTime = Item.useAnimation = 17;
+            Item.useTurn = true;
+            Item.maxStack = Item.CommonMaxStack;
+
+            //Item.potionDelay = 2100;
+            //Item.potion = true;
+
+            Item.consumable = true;
+            Item.useStyle = ItemUseStyleID.DrinkLiquid;
+            Item.consumable = true;
+            Item.noMelee = true;
+
+            Item.value = Item.sellPrice(silver: 2);
+            Item.rare = ItemRarityID.Green;
+
+            Item.UseSound = SoundID.Item3;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(2)
+                .AddIngredient(ItemID.BatBat, 1)
+                .AddIngredient(ItemID.Bottle, 1)
+                .AddTile(TileID.Bottles)
+                .Register();
+
+            CreateRecipe(2)
+                .AddIngredient(ItemID.BatBat, 1)
+                .AddIngredient(ItemID.Bottle, 1)
+                .AddTile(TileID.AlchemyTable)
+                .Register();
+
+            CreateRecipe(5)
+                .AddIngredient(ItemID.BatBat, 1)
+                .AddIngredient(ItemID.Bottle, 1)
+                .AddTile(TileID.ImbuingStation)
+                .Register();
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if (player.HasBuff(BuffID.Rabies)) player.ClearBuff(BuffID.Rabies);
+            return base.UseItem(player);
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.HasBuff(BuffID.Rabies)) return true;
+            return false;
+        }
+    }
+}
---
+++ Content/Items/Placeable/AetherPylon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using VanillaModding.Content.Tiles;
+
+namespace VanillaModding.Content.Items.Placeable
+{
+    internal class AetherPylon : ModItem
+    {
+        public override void SetDefaults()
+        {
+            // Basically, this a just a shorthand method that will set all default values necessary to place
+            // the passed in tile type; in this case, the Example Pylon tile.
+            Item.DefaultToPlaceableTile(ModContent.TileType<AetherPylonTile>());
+
+            // Another shorthand method that will set the rarity and how much the item is worth.
+            Item.SetShopValues(ItemRarityColor.Blue1, Terraria.Item.buyPrice(gold: 10));
+        }
+    }
+}
