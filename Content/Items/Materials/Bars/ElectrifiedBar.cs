eada3a50844dace2714498528d63637a9952da28
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2026-07-14 23:31:17 -0700

    bar test

--- Content/Items/Materials/Bars/ElectrifiedBar.cs
+++ Content/Items/Materials/Bars/ElectrifiedBar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using VanillaModding.Content.Tiles.Bars;
 
 namespace VanillaModding.Content.Items.Materials.Bars
 {
@@ -19,16 +21,28 @@ namespace VanillaModding.Content.Items.Materials.Bars
             ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 
             Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
+            ItemID.Sets.SortingPriorityMaterials[Type] = 59;
+            ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
         }
 
         public override void SetDefaults()
         {
             Item.width = 38;
             Item.height = 32;
+            Item.consumable = true;
             Item.maxStack = Item.CommonMaxStack;
 
             Item.value = Item.sellPrice(gold: 3, silver: 50);
             Item.rare = ItemRarityID.Yellow;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+
+            Item.createTile = ModContent.TileType<ElectrifiedBarTile>();
+            Item.placeStyle = 0;
         }
 
         public override void AddRecipes()
@@ -39,5 +53,10 @@ namespace VanillaModding.Content.Items.Materials.Bars
                 .AddTile(TileID.MythrilAnvil)
                 .Register();
         }
+
+        public override void PostUpdate()
+            => Lighting.AddLight(Item.Center, new Color(140, 249, 255).ToVector3() * 0.45f * Main.essScale);
+        public override Color? GetAlpha(Color lightColor)
+            => new Color(1f * 0.97f, 1f * 0.97f, 1f * 0.99f, 0.87f);
     }
 }
---
+++ Content/Tiles/Bars/ElectrifiedBarTile.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace VanillaModding.Content.Tiles.Bars
+{
+    internal class ElectrifiedBarTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileShine[Type] = 1100;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(255, 251, 232), Language.GetText("MapObject.MetalBar"));
+        }
+    }
+}
